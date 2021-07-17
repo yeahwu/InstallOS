bf0368c115152ed59ca85f1d2c7d7a2fe252fc2e
yeahwu <yeahwu@users.noreply.github.com>
2021-07-17 23:48:53 +0800

    Create InstallOS.sh

---
+++ InstallOS.sh
@@ -0,0 +1,198 @@
+#!/bin/sh
+## Default root password: 111111.online
+
+if [[ $EUID -ne 0 ]]; then
+    clear
+    echo "Error: This script must be run as root!" 1>&2
+    exit 1
+fi
+
+if [ -f "/usr/bin/apt-get" ];then
+	isDebian=`cat /etc/issue|grep Debian`
+	if [ "$isDebian" != "" ];then
+		apt-get install -y xz-utils openssl gawk file curl
+		apt install -y xz-utils openssl gawk file curl
+		sleep 3s
+	else
+		apt-get install -y xz-utils openssl gawk file curl
+		apt install -y xz-utils openssl gawk file curl
+		sleep 3s
+	fi
+else
+    yum install -y xz openssl gawk file curl
+    sleep 3s
+fi
+
+function CopyRight() {
+  clear
+  echo "----------------------------------------------"
+  echo ""
+  echo "Blog: https://111111.online"
+  echo ""
+  echo "TG Channel: https://t.me/yeahwu_channel"
+  echo ""
+  echo "Supports DD to Ubuntu and Debian systems only."
+  echo ""
+  echo "----------------------------------------------"
+  echo -e "\n"
+}
+
+function isValidIp() {
+  local ip=$1
+  local ret=1
+  if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+    ip=(${ip//\./ })
+    [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
+    ret=$?
+  fi
+  return $ret
+}
+
+function ipCheck() {
+  isLegal=0
+  for add in $MAINIP $GATEWAYIP $NETMASK; do
+    isValidIp $add
+    if [ $? -eq 1 ]; then
+      isLegal=1
+    fi
+  done
+  return $isLegal
+}
+
+function GetIp() {
+  MAINIP=$(ip route get 1 | awk -F 'src ' '{print $2}' | awk '{print $1}')
+  GATEWAYIP=$(ip route | grep default | awk '{print $3}' | head -1)
+  SUBNET=$(ip -o -f inet addr show | awk '/scope global/{sub(/[^.]+\//,"0/",$4);print $4}' | head -1 | awk -F '/' '{print $2}')
+  value=$(( 0xffffffff ^ ((1 << (32 - $SUBNET)) - 1) ))
+  NETMASK="$(( (value >> 24) & 0xff )).$(( (value >> 16) & 0xff )).$(( (value >> 8) & 0xff )).$(( value & 0xff ))"
+}
+
+function UpdateIp() {
+  read -r -p "Your IP: " MAINIP
+  read -r -p "Your Gateway: " GATEWAYIP
+  read -r -p "Your Netmask: " NETMASK
+}
+
+function SetNetwork() {
+  isAuto='0'
+  if [[ -f '/etc/network/interfaces' ]];then
+    [[ ! -z "$(sed -n '/iface.*inet static/p' /etc/network/interfaces)" ]] && isAuto='1'
+    [[ -d /etc/network/interfaces.d ]] && {
+      cfgNum="$(find /etc/network/interfaces.d -name '*.cfg' |wc -l)" || cfgNum='0'
+      [[ "$cfgNum" -ne '0' ]] && {
+        for netConfig in `ls -1 /etc/network/interfaces.d/*.cfg`
+        do
+          [[ ! -z "$(cat $netConfig | sed -n '/iface.*inet static/p')" ]] && isAuto='1'
+        done
+      }
+    }
+  fi
+}
+
+function NetMode() {
+  CopyRight
+
+  if [ "$isAuto" == '0' ]; then
+    read -r -p "Using DHCP to configure network automatically? [Y/n]:" input
+    case $input in
+      [yY][eE][sS]|[yY]) NETSTR='' ;;
+      [nN][oO]|[nN]) isAuto='1' ;;
+      *) NETSTR='' ;;
+    esac
+  fi
+
+  if [ "$isAuto" == '1' ]; then
+    GetIp
+    ipCheck
+    if [ $? -ne 0 ]; then
+      echo -e "Error occurred when detecting ip. Please input manually.\n"
+      UpdateIp
+    else
+      CopyRight
+      echo "IP: $MAINIP"
+      echo "Gateway: $GATEWAYIP"
+      echo "Netmask: $NETMASK"
+      echo -e "\n"
+      read -r -p "Confirm? [Y/n]:" input
+      case $input in
+        [yY][eE][sS]|[yY]) ;;
+        [nN][oO]|[nN])
+          echo -e "\n"
+          UpdateIp
+          ipCheck
+          [[ $? -ne 0 ]] && {
+            clear
+            echo -e "Input error!\n"
+            exit 1
+          }
+        ;;
+        *) clear; echo "Canceled by user!"; exit 1;;
+      esac
+    fi
+    NETSTR="--ip-addr ${MAINIP} --ip-gate ${GATEWAYIP} --ip-mask ${NETMASK}"
+  fi
+}
+
+function Start() {
+  CopyRight
+
+  isCN='0'
+  geoip=$(wget --no-check-certificate -qO- https://api.ip.sb/geoip -T 10 | grep "\"country_code\":\"CN\"")
+  if [[ "$geoip" != "" ]];then
+    isCN='1'
+  fi
+
+  if [ "$isAuto" == '0' ]; then
+    echo "Using DHCP mode."
+  else
+    echo "IP: $MAINIP"
+    echo "Gateway: $GATEWAYIP"
+    echo "Netmask: $NETMASK"
+  fi
+
+  [[ "$isCN" == '1' ]] && echo "Using domestic mode."
+
+  if [ -f "/tmp/InstallNET.sh" ]; then
+    rm -f /tmp/InstallNET.sh
+  fi
+  wget --no-check-certificate -qO /tmp/InstallNET.sh 'https://111111.online/cloud/docs/InstallNET.sh' && chmod a+x /tmp/InstallNET.sh
+
+  DMIRROR=''
+  UMIRROR=''
+  if [[ "$isCN" == '1' ]];then
+    DMIRROR="--mirror http://mirrors.aliyun.com/debian/"
+    UMIRROR="--mirror http://mirrors.aliyun.com/ubuntu/"
+  fi
+
+  echo -e "\nPlease select an OS:"
+  echo "  1) Debian 9"
+  echo "  2) Debian 10"
+  echo "  3) Ubuntu 18.04"
+  echo "  4) Ubuntu 20.04"
+  echo "  5) Custom image"
+  echo "  0) Exit"
+  echo -ne "\nYour option: "
+  read N
+  case $N in
+    1) echo -e "\nPassword: 111111.online\n"; read -s -n1 -p "Press any key to continue..." ; bash /tmp/InstallNET.sh -d 9 -v 64 -a $NETSTR $DMIRROR ;;
+    2) echo -e "\nPassword: 111111.online\n"; read -s -n1 -p "Press any key to continue..." ; bash /tmp/InstallNET.sh -d 10 -v 64 -a $NETSTR $DMIRROR ;;
+    3) echo -e "\nPassword: 111111.online\n"; read -s -n1 -p "Press any key to continue..." ; bash /tmp/InstallNET.sh -u 18.04 -v 64 -a $NETSTR $UMIRROR ;;
+    4) echo -e "\nPassword: 111111.online\n"; read -s -n1 -p "Press any key to continue..." ; bash /tmp/InstallNET.sh -u 20.04 -v 64 -a $NETSTR $UMIRROR ;;
+    5)
+      echo -e "\n"
+      read -r -p "Custom image URL: " imgURL
+      echo -e "\n"
+      read -r -p "Are you sure start reinstall? [Y/n]: " input
+      case $input in
+        [yY][eE][sS]|[yY]) bash /tmp/InstallNET.sh $NETSTR -dd $imgURL $DMIRROR ;;
+        *) clear; echo "Canceled by user!"; exit 1;;
+      esac
+      ;;
+    0) exit 0;;
+    *) echo "Wrong input!"; exit 1;;
+  esac
+}
+
+SetNetwork
+NetMode
+Start

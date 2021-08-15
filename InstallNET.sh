2c92aadecb1ba1cc8c2b05ee3d8388ea6ef16bd1
yeahwu <yeahwu@users.noreply.github.com>
2021-08-15 22:36:47 +0800

    Update InstallNET.sh

--- InstallNET.sh
+++ InstallNET.sh
@@ -671,29 +671,19 @@ cp -f '/net.bat' './net.bat'; \
 /sbin/reboot; \
 debconf-set grub-installer/bootdev string "\$(list-devices disk |head -n1)"; \
 umount /media || true; \
-d-i partman-auto/disk string
+d-i partman/mount_style select uuid
+d-i partman-auto/init_automatically_partition select Guided - use entire disk
+d-i partman-auto/choose_recipe select All files in one partition (recommended for new users)
 d-i partman-auto/method string regular
-d-i partman-auto/expert_recipe string         \
-   boot-root ::                               \
-      40 50 160 ext4                          \
-         $primary{ } $bootable{ }             \
-         method{ format } format{ }           \
-         use_filesystem{ } filesystem{ ext4 } \
-         mountpoint{ /boot } .                \
-      500 1000 -1 ext4                	      \
-         $primary{ }                          \
-         method{ format } format{ }           \
-         use_filesystem{ } filesystem{ ext4 } \
-         mountpoint{ / } .                    \
-      64 512 100% linux-swap                  \
-         method{ swap } format{ } .
+d-i partman-lvm/device_remove_lvm boolean true
+d-i partman-md/device_remove_md boolean true
+d-i partman-auto/choose_recipe select atomic
 d-i partman-partitioning/confirm_write_new_label boolean true
-d-i partman-auto/choose_recipe select boot-root
 d-i partman/choose_partition select finish
+d-i partman-lvm/confirm boolean true
+d-i partman-lvm/confirm_nooverwrite boolean true
 d-i partman/confirm boolean true
 d-i partman/confirm_nooverwrite boolean true
-d-i partman/default_filesystem string ext4
-d-i partman/mount_style select uuid
 d-i debian-installer/allow_unauthenticated boolean true
 tasksel tasksel/first multiselect minimal
 d-i pkgsel/update-policy select none
@@ -707,6 +697,7 @@ d-i pkgsel/upgrade select none
 popularity-contest popularity-contest/participate boolean false
 d-i grub-installer/only_debian boolean true
 d-i grub-installer/bootdev string default
+d-i grub-installer/force-efi-extra-removable boolean true
 d-i finish-install/reboot_in_progress note
 d-i debian-installer/exit/reboot boolean true
 d-i preseed/late_command string	\
